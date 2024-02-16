e02cc649c9ef0899c2d1d3796ebb19e699ed065a
Daniel Vander-Hyde <dvande72hyde@hotmail.com>
2024-02-16 10:59:23 -0500

    Adding more plugins

--- init.lua
+++ init.lua
@@ -1,1 +1,18 @@
-print('hello there')
+require('git.plugins')
+require('git.telescope')
+require('git.treesitter')
+require('git.undotree')
+
+vim.cmd('set number')
+vim.cmd('filetype plugin indent on')
+vim.cmd('syntax enable')
+
+vim.g.vimtex_view_method='zathura'
+vim.g.latex_view_general_viewer='zathura'
+
+vim.g.vimtex_compiler_method='latexmk'
+
+vim.keymap.set('n', '<F4>', ':nohl<CR>')
+
+vim.keymap.set('n', '<C-t>', ':Ex<CR>')
+
